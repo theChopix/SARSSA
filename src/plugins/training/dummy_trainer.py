1545d9e200b0c7c1641dd20164d375b2101b4ca2
thechopix <kratkyjan244@gmail.com>
2025-12-14 17:32:17 +0100

    chore: renaming folders with sae and cfm trainers

--- src/plugins/training/dummy_trainer.py
+++
@@ -1,8 +0,0 @@
-from plugins.plugin_interface import BasePlugin
-from app.utils.logger import logger
-
-class Plugin(BasePlugin):
-    def run(self, context):
-        logger.info("Running dummy training...")
-        context["model"] = {"status": "trained", "accuracy": 0.85}
-        return context
