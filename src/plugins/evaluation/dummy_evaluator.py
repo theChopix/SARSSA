4198353cf239fd3480a751f37dafc2f9bf60aa20
thechopix <kratkyjan244@gmail.com>
2026-03-01 15:32:54 +0100

    :bug: fix dummy evaluator to be plgn cls

--- src/plugins/evaluation/dummy_evaluator.py
+++ src/plugins/evaluation/dummy_evaluator.py
@@ -1,6 +1,7 @@
+from plugins.plugin_interface import BasePlugin
 from app.utils.logger import logger
 
-class Plugin:
+class Plugin(BasePlugin):
     def run(self, context):
         logger.info("Running dummy evaluation...")
         context["evaluation"] = {"RMSE": 0.12}
