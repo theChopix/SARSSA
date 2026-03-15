504688b8e55cfb99b4a933da15a697de4d164b3e
thechopix <kratkyjan244@gmail.com>
2026-03-15 14:52:07 +0100

    dummy_evalutor removed

--- src/plugins/evaluation/dummy_evaluator.py
+++
@@ -1,8 +0,0 @@
-from plugins.plugin_interface import BasePlugin
-from app.utils.logger import logger
-
-class Plugin(BasePlugin):
-    def run(self, context):
-        logger.info("Running dummy evaluation...")
-        context["evaluation"] = {"RMSE": 0.12}
-        return context
